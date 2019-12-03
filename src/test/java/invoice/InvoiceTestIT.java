3a0b561c5856bed39fc1a25f5eb8d38312ff991f
Martin Blomberg <martin.blomberg@outlook.com>
2019-12-03 20:38:50 +0100

    Maven pom file added. IntegrationTest file added as InvoiceTestIT.java

--- src/test/java/invoice/InvoiceTestIT.java
+++ src/test/java/invoice/InvoiceTestIT.java
@@ -4,5 +4,13 @@ package invoice;
 /* An integration test verifies the workings of a complete program,
  a module, or a set of dependant classes.*/
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class InvoiceTestIT {
+    @Test
+    void integrationTest(){
+        fail("This integration test is supposed to fail. Must be implemented for real soon.");
+    }
 }
