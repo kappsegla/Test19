16182210bd8e4c0bbd26e9b8abf6f1aae6256cb6
Martin Blomberg <martin.blomberg@outlook.com>
2019-12-03 20:13:27 +0100

    Maven pom file added. IntegrationTest file added as InvoiceTestIT.java

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "html:output", features = "src/test/resources/cucumber")
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/cucumber")
 public class RunCucumberTest
 {
 
---
+++ src/test/java/invoice/InvoiceTestIT.java
@@ -0,0 +1,8 @@
+package invoice;
+
+
+/* An integration test verifies the workings of a complete program,
+ a module, or a set of dependant classes.*/
+
+public class InvoiceTestIT {
+}
