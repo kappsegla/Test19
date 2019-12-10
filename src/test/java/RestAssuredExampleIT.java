42db29c79eb6eab930f1bf4531946dfd5e7b8803
Martin Blomberg <martin.blomberg@outlook.com>
2019-12-10 12:08:31 +0100

    Added RestAssuredExampleIT.java

---
+++ src/test/java/RestAssuredExampleIT.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class RestAssuredExampleIT {
+
+    @Test
+    void checkAstronautsInSpace(){
+        get("http://api.open-notify.org/astros.json").then()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("message", equalTo("success"),
+                        "number", equalTo(6));
+    }
+}
